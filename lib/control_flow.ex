1debb7797a8b8b247643b08e6c7ced70d14be255
Travis Elnicky <travis.elnicky@gmail.com>
2019-04-08 17:13:16 -0600

    Step 2: COND

--- lib/control_flow.ex
+++ lib/control_flow.ex
@@ -10,4 +10,20 @@ defmodule ControlFlow do
       number
     end
   end
+
+  def upto(n) when n > 0, do: _upto(1, n, [])
+
+  defp _upto(_current, 0, result), do: Enum.reverse(result)
+
+  defp _upto(current, left, result) do
+    next_answer =
+      cond do
+        rem(current, 3) == 0 and rem(current, 5) == 0 ->
+          "fizzbuzz"
+        true ->
+          current
+      end
+
+    _upto(current + 1, left - 1, [ next_answer | result ])
+  end
 end
--- test/control_flow_test.exs
+++ test/control_flow_test.exs
@@ -12,4 +12,11 @@ defmodule ControlFlowTest do
       assert result == 2
     end
   end
+
+  describe "ControlFlow.fizzbuzz" do
+    test "it returns the correct result up to 20" do
+      result = ControlFlow.upto(20)
+      assert result == [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz", 16, 17, "fizz", 19, "buzz"]
+    end
+  end
 end
