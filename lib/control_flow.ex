c3f30aba070fcac54e6257129dccfafa34094769
Travis Elnicky <travis.elnicky@gmail.com>
2019-04-08 20:10:44 -0600

    Step 3: CASE SOL

--- lib/control_flow.ex
+++ lib/control_flow.ex
@@ -10,9 +10,23 @@ defmodule ControlFlow do
   def fizzbuzz(number) do
     case ApiClient.fetch(number) do
       {:ok, result} ->
-        result
+        get_fizzbuzz(result)
       {:error, error} ->
         error
     end
   end
+
+  ##
+  # Private
+  #
+
+  defp get_fizzbuzz(response) do
+    case response do
+      %{fizzbuzz: true} -> "fizzbuzz"
+      %{fizz: true} -> "fizz"
+      %{buzz: true} -> "buzz"
+      fizzbuzz -> fizzbuzz.number
+    end
+  end
 end
+
